5f9cff44df47e0b5c9c110fa2fbccd58a8d1ef90
cooleymike <cooley.mike1@gmail.com>
2015-06-17 16:47:21 -0500

    Init Commit

--- tmnt_script.js
+++ tmnt_script.js
@@ -1,133 +1,106 @@
-﻿//=============MAIN FIREBASE===================================
+﻿var turtles=[];
 
-//============================================================  
-//Our To Do Constructor
-var User = function (task, chirps) {
-    this.task = task,
-    this.chirps = chirps
+function turtle(TurtleName, TurtleWeapon, TurtleColor){
+    this.name = TurtleName;
+    this.weapon = TurtleWeapon;
+    this.color = TurtleColor;
 }
-//Central Data Container/Array
-var todoArray = [];
-//This variable will keep track of current item
-var currId;
 
-//Fetch data from Firebase 
-var fetch = function (callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", "https://watchthis.firebaseio.com/.json", true);
-    xhr.onload = function () {
-        if (this.status >= 200 && this.status < 400) {
-            console.log(this.response)
-            callback(JSON.parse(this.response))
-        }
-    }
-    xhr.send();
-}
-fetch(showresults);
-fetch(showresultstopanel);
 
-//Callback for fetch
-function showresults(data) {
-    var indexCounter = 0;
-    var htmlstr = "<table class='table table-bordered table-condensed table-striped'>";
-    for (var i in data) {
-        console.log(data[i].User, data[i].chirp);
-        todoArray.push({
-            id: i,
-            User: data[i].User,
-            chirp: data[i].chirp
-        })
+// general placeholder
+var guid;
 
-        htmlstr += "<tr><td>";
-        htmlstr += "<td>" + data[i].User + "</td>" + "<td>" + data[i].chirp + "</td>";
-        htmlstr += "<td><button onclick='dodeletetodo(" + indexCounter + ")' class='btn btn-xs btn-danger'><span class='glyphicon glyphicon-scissors'></span></button></td>";
-        // htmlstr += "<button onclick='doupdatetodo(" + indexCounter + ")' class='btn btn-xs btn-warning'><span class='glyphicon glyphicon-erase'></span></button>";
-        htmlstr += "</td></tr>";
-        indexCounter++;
-    }
-    htmlstr += "</table>"
-    $("#ToDoList").html(htmlstr);
-}
-function showresultstopanel(data) {
+////
+$('#target').click(function(){
+    var name = $('#AddTurtleName').val();
+    var weapon = $('#AddTurtleWeapon').val();
+    var color = $('#AddTurtleColor').val();  
+    var newTurtle = new turtle(name, weapon, color);
 
-    var indexCounter = 0;
-    var htmlstrtopanel = "";
-    var htmlstrtopanelName = "";
-    for (var i in data) {
-        console.log(data[i].User, data[i].chirp);
-        todoArray.push({
-            id: i,
-            User: data[i].User,
-            chirp: data[i].chirp
-        })a 
+    turtles.push(newTurtle);
+    console.log(turtles.sort(function (a, b) { return a.name > b.name; }));
+    var tRtl = $('#newturtle');
+    tRtl.empty();
+    for (var i = 0; i < turtles.length; i++) {
+        var tds = '<tr><td>' + turtles[i].name + '</td><td>' + turtles[i].weapon + '</td><td>' + turtles[i].color + '</td><td>' + "   " + '<button class="btn btn-info" onclick="editTurtle(' + i + ')">Edit</button>' + "   " + '<button class="btn btn-danger" onclick="Deleteturtle(' + i + ')">Delete</button>' + "   " + '<button class="btn btn-success" onclick="AJAX(' + i + ')">Add to Firebase</button></td></tr>';
 
-        htmlstrtopanel += "<span class='chat-img pull-left'><img src='http://salmonriverresources.com/wp-content/uploads/2015/03/hawaiian-islands-beach-sunset-GwWz-50x50.jpg' alt='User Avatar' class='img-circle' /></span>";
-        htmlstrtopanel += "<strong class='primary-font'>" + data[i].User + "</strong>";
-        $("#myChirp").html(htmlstrtopanel);
-        htmlstrtopanel += "<small class='pull-right text-muted'><span class='glyphicon glyphicon-time'></span>12 mins ago</small>";
-        htmlstrtopanel += "<li><br/>" + data[i].chirp + "</li>";
-        //htmlstr += "<td><button onclick='dodeletetodo(" + indexCounter + ")' class='btn btn-xs btn-danger'><span class='glyphicon glyphicon-scissors'></span></button></td>";
-        // htmlstr += "<button onclick='doupdatetodo(" + indexCounter + ")' class='btn btn-xs btn-warning'><span class='glyphicon glyphicon-erase'></span></button>";
-
-        indexCounter++;
+        tRtl.append(tds);
+        console.log("this button is working")
     }
 
+})
+// Ajax post
+function AJAX(i) {
+    console.log("working");
+    var xhr = new XMLHttpRequest(); 
+    xhr.open("POST", "https://tmnturtles.firebaseio.com/.json", true);
+    xhr.onload = function() {
+        if(this.status >=200 && this.status < 400){
+            console.log("success");
+        } else {
+            console.log("error on send");
 
-    $("#myChirp").html(htmlstrtopanel);
-
-}
-
-
-
-function doaddtodo() {
-    console.log($("#todotext").val())
-    console.log($("#todocompleted").val())
-    var newtodo = {
-        User: $("#todotext").val(),
-        chirp: $("#todocompleted").val()
+        }
     }
-    sendToAjax(newtodo)
-}
-//Send to Ajax
-function sendToAjax(objToPost) {
-    var request = new XMLHttpRequest();
-    request.open('POST', 'https://watchthis.firebaseio.com/.json', true);
-    request.onload = function () {
-        if (this.status >= 200 && this.status < 400) {
-            fetch(showresults);
-            fetch(showresultstopanel);
-
-        }
-        else {
-            //Do Something On Error
-        }
-    };
-    request.send(JSON.stringify(objToPost));
+    xhr.send(JSON.stringify(turtles[i]));
 }
 
-//Delete Chirps
-function dodeletetodo(index) {
-    console.log("Index to delete:", index);
-    console.log(todoArray[index])
-    var xhr = new XMLHttpRequest();
-    xhr.open("DELETE", "https://watchthis.firebaseio.com/" + todoArray[index].id + "/.json", true)
-    xhr.send();
-    xhr.onload = function () {
-        if (this.status >= 200 && this.status < 400) {
-            fetch(showresults);
-            fetch(showresultstopanel);
-        }
-        else {
-            //Do Something On Error
+         //Save
+        function doSave(){
+            turtles[guid].name = $("#editName").val();
+            turtles[guid].weapon = $("#editWeapon").val();
+            turtles[guid].color = $("#editColor").val();
+            var tRes = $("#newturtle");
+            console.log("working");
+            tRes.empty();
+            for (var i = 0; i < turtles.length; i++){
+        
+                var dis = '<tr><td>' + turtles[i].name + '</td><td>' + turtles[i].weapon + '</td><td>' + turtles[i].color + '</td><td>' + "   " + '<button class="btn btn-info" onclick="editTurtle(' + i + ')">Edit</button>' + "   " + '<button class="btn btn-danger" onclick="Deleteturtle(' + i + ')">Delete</button>' + "   " + '<button class="btn btn-success" onclick="AJAX(' + i + ')">Save to Firebase</button></td></tr>';
+                tRes.append(dis);
+            }
         }
 
-    };
-}
-//POLLING (reason why need to keep refreshing)
-function init() {
-    fetch(showresults);
-    fetch(showresultstopanel);
-    myTimer = setInterval(fetch(showresults), 1000);
-    myTimer2 = setInterval(fetch(showresultstopanel), 1000);
-}
-init();
+        
+
+
+
+        //Edit
+        var turtleUpdate;
+        function editTurtle(tempid){
+            turtleUpdate = tempid;
+            guid = tempid;
+            var tempTurtle = turtles[tempid];
+            $("#editName").val(tempTurtle.name);
+            $("#editWeapon").val(tempTurtle.weapon);
+            $("#editColor").val(tempTurtle.color);
+    
+            $("#myEditModal").modal("show");
+            console.log(tempTurtle);
+            console.log(turtleUpdate);
+            var tRes = $("#newturtle");
+            tRes.empty();
+            for(var i = 0; i < turtles.length; i++){
+
+                var dis = '<tr><td>' + turtles[i].name + '</td><td>' + turtles[i].weapon + '</td><td>' + turtles[i].color + '</td><td>' + "   " + '<button class="btn btn-info" onclick="editTurtle(' + i + ')">Edit</button>' + "   " + '<button class="btn btn-danger" onclick="Deleteturtle(' + i + ')">Delete</button>' + "   " + '<button class="btn btn-success" onclick="AJAX(' + i + ')">Save to Firebase</button></td></tr>';
+                tRes.append(dis);
+            }
+        }
+        
+        //Delete
+        function Deleteturtle(i) {
+            turtles.splice(i, 1);
+            var tRes = $('#newturtle');
+            tRes.empty();
+            console.log("workingbutton")
+            for (var i = 0; i < turtles.length; i++) {
+
+                var dis = '<tr><td>' + turtles[i].name + '</td><td>' + turtles[i].weapon + '</td><td>' + turtles[i].color + '</td><td>' + "   " + '<button class="btn btn-info" onclick="editTurtle(' + i + ')">Edit</button>' + "   " + '<button class="btn btn-danger" onclick="Deleteturtle(' + i + ')">Delete</button>' + "   " + '<button class="btn btn-success" onclick="AJAX(' + i + ')">Save to Firebase</button></td></tr>';
+                tRes.append(dis);
+            }
+        }
+
+
+
+
+
+    
